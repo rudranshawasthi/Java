7f8070ae3c210d8e45957c17d053296b3d571b9c
rudransh <rudranshofficial@gmail.com>
2020-01-26 01:13:32 +0530

    --

--- Coding_Ninjas/Language_Tools_Assignment/Find_the_Unique_Element.java
+++ Coding_Ninjas/Language_Tools_Assignment/Find_the_Unique_Element.java
@@ -1,4 +1,9 @@
 package Coding_Ninjas.Language_Tools_Assignment;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Scanner;
+
 /*
     Find the Unique Element
         Given an integer array of size 2N + 1. In this given array, N numbers are
@@ -25,4 +30,28 @@ package Coding_Ninjas.Language_Tools_Assignment;
         1
  */
 public class Find_the_Unique_Element {
+    public static void main(String ar[]){
+        Scanner z =new Scanner(System.in);
+        int n = z.nextInt();
+        HashSet<Integer> hs =new HashSet<>();
+        int i,a;
+        for (i=0;i<n;i++) {
+            try {
+               a = z.nextInt();
+            } catch (Exception E) {
+                break;
+            }
+            if(hs.contains(a)){
+                hs.remove(a);
+            }
+            else{
+                hs.add(a);
+            }
+        }
+        Iterator<Integer> o=hs.iterator();
+        while(o.hasNext())
+        {
+            System.out.println(o.next());
+        }
+    }
 }
