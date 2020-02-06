43af241ed869f1b17c7898a0e14a88d2d0652a68
rudransh <rudranshofficial@gmail.com>
2020-02-06 23:36:39 +0530

    --

--- Coding_Ninjas/Advanced_Recursion/Merge_Sort_Code.java
+++ Coding_Ninjas/Advanced_Recursion/Merge_Sort_Code.java
@@ -1,4 +1,63 @@
 package Coding_Ninjas.Advanced_Recursion;
 
+import java.util.Scanner;
+
 public class Merge_Sort_Code {
+
+    static void mergeSort(int A[],int left,int right){
+        if(right > left){
+            int mid = (left + right)/2;
+
+            mergeSort(A,left,mid);
+            mergeSort(A,mid+1,right);
+            merge(A,left,mid+1,right);
+        }
+    }
+    static void merge(int A[],int left,int mid,int right) {
+        int i = left, j = mid, k = 0;
+
+        int[] temp = new int[right - left + 1];
+        while (i < mid && j <= right) {
+            if (A[i] <= A[j]) {
+                temp[k++] = A[i++];
+            } else {
+                temp[k++] = A[j++];
+            }
+        }
+        while (i < mid) {
+            temp[k++] = A[i++];
+        }
+        while (j <= right) {
+            temp[k++] = A[j++];
+        }
+
+        for (i = left, k = 0; i <= right; i++, k++) {
+            A[i] = temp[k];
+        }
+    }
+    public static void mergeSort(int[] input){
+        // Write your code here
+        mergeSort(input,0,input.length-1);
+    }
+    public static int[] takeInput() {
+        Scanner s = new Scanner(System.in);
+        int size = s.nextInt();
+        int arr[] = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = s.nextInt();
+        }
+        return arr;
+    }
+
+    public static void printArray(int input[]) {
+        for(int i = 0; i < input.length; i++) {
+            System.out.print(input[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] input = takeInput();
+        mergeSort(input);
+        printArray(input);
+    }
 }
--- Coding_Ninjas/Advanced_Recursion/Quick_Sort_Code.java
+++ Coding_Ninjas/Advanced_Recursion/Quick_Sort_Code.java
@@ -1,4 +1,68 @@
 package Coding_Ninjas.Advanced_Recursion;
 
+import java.util.Scanner;
+
 public class Quick_Sort_Code {
+    static Scanner s = new Scanner(System.in);
+
+    public static int[] takeInput(){
+        int size = s.nextInt();
+        int[] input = new int[size];
+        for(int i = 0; i < size; i++){
+            input[i] = s.nextInt();
+        }
+        return input;
+    }
+
+    public static void main(String[] args) {
+        int[] input = takeInput();
+        quickSort(input);
+        for(int i = 0; i < input.length; i++) {
+            System.out.print(input[i] + " ");
+        }
+    }
+    public static void quickSort(int[] input){
+        quickSort(input,0,input.length-1);
+    }
+    public static void quickSort(int[] input,int startingIndex,int endIndex){
+        if(endIndex <= startingIndex){
+            return;
+        }
+        int partitionIndex = partition(input,startingIndex,endIndex);
+        quickSort(input,startingIndex,partitionIndex-1);
+        quickSort(input,partitionIndex,endIndex);
+
+    }
+    public static int partition(int[] input, int startIndex, int endIndex){
+        int piviot = input[startIndex];
+        int count = 0;
+        for (int i = startIndex; i < endIndex; i++)
+        {
+            if (input[i] <= piviot)
+            {
+                count++;
+            }
+        }
+        int piviotIndex = startIndex + count;
+        input[startIndex] = input[piviotIndex];
+        input[piviotIndex] = piviot;
+
+        int i = startIndex, j = endIndex;
+        while (i < j) {
+            while (i < endIndex && input[i] <= piviot) {
+                i++;
+            }
+            while (input[j] > piviot) {
+                j--;
+            }
+            if (i <= j) {
+                int temp = input[i];
+                input[i] = input[j];
+                input[j] = temp;
+                i++;
+                j--;
+            }
+        }
+        return piviotIndex;
+    }
 }
